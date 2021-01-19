ed3db29c9dbc280eab8e2dc0b0ad76eebfd6c7e6
Azad Yaşar <ceazadyasar@gmail.com>
2021-01-19 21:59:58 +0300

    Fix import

--- src/gpt2/data/__init__.py
+++ src/gpt2/data/__init__.py
@@ -1,4 +1,4 @@
 from gpt2.data.dataset import Dataset
 from gpt2.data.vocabulary import VocabSP, VocabYTTM
-from gpt2.data.tokenization import Tokenizer
+# from gpt2.data.tokenization import Tokenizer
 from gpt2.data.corpus import TokenizedCorpus
